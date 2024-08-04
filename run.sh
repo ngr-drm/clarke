a92e3b28a2118384ddbdbbbb06a4fbe26e0fb9ea
nrg-drm <su.re44e@gmail.com>
2024-08-04 19:37:21 -0300

    ci: try fix action

--- run.sh
+++ run.sh
@@ -16,6 +16,9 @@ then
   echo "running migrations..."
   pnpm run migrations:docker & PID=$!
   wait $PID
+  echo "running tests..."
+  pnpm run test:e2e:docker & PID=$!
+  wait $PID
   echo "running docker server..."
   pnpm run dev:docker 
 fi
@@ -25,9 +28,10 @@ then
   echo "running migrations..."
   pnpm run migrations & PID=$!
   wait $PID
-
   echo "running sandbox server..."
   pnpm run start 
+
+
 fi
 
 
