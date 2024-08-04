a46fa836c16d5606d89b7bde108e2a5b59280700
nrg-drm <su.re44e@gmail.com>
2024-08-04 18:42:07 -0300

    ci: try fix action

--- run.sh
+++ run.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
-
-echo "running migrations..."
-
-pnpm run migrations & PID=$!
-
-wait $PID
-
 echo "validating enviroment..."
 
 if [ $API_ENV == "local" ] 
 then
+  echo "running migrations..."
+  pnpm run migrations & PID=$!
+  wait $PID
+
   echo "running local server..."
   pnpm run dev:watch
 fi
 
 if [ $API_ENV == "docker" ] 
 then
-  echo "running containers..."
+  echo "running migrations..."
+  pnpm run migrations:docker & PID=$!
+  wait $PID
+  echo "running docker server..."
   pnpm run dev:docker 
 fi
 
 if [ $API_ENV == "sandbox" ] 
 then
+  echo "running migrations..."
+  pnpm run migrations & PID=$!
+  wait $PID
+
   echo "running sandbox server..."
   pnpm run start 
 fi
