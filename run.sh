da1e5904017abdad531b4d59f4d056e73616deda
nrg-drm <su.re44e@gmail.com>
2024-08-04 17:47:34 -0300

    ci: try fix action

--- run.sh
+++ run.sh
@@ -11,11 +11,20 @@ echo "validating enviroment..."
 if [ $API_ENV == "local" ] 
 then
   echo "running local server..."
-  pnpm run dev:watch & PID=$!
-  wait $PID
-else
-  echo "running sandbox server..."
-  pnpm run start & PID=$!
-  wait $PID
+  pnpm run dev:watch
 fi
 
+if [ $API_ENV == "docker" ] 
+then
+  echo "running containers..."
+  pnpm run dev:docker 
+fi
+
+if [ $API_ENV == "sandbox" ] 
+then
+  echo "running sandbox server..."
+  pnpm run start 
+fi
+
+
+\
