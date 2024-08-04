4941d0d1b00b3c852c69ce512e1599c1f89bca50
nrg-drm <su.re44e@gmail.com>
2024-08-04 19:59:53 -0300

    ci: try fix action

--- run.sh
+++ run.sh
@@ -9,6 +9,8 @@ then
 
   echo "running local server..."
   pnpm run dev:watch
+
+  echo "ENV: $API_ENV is up"
 fi
 
 if [ $API_ENV == "docker" ] 
@@ -16,11 +18,11 @@ then
   echo "running migrations..."
   pnpm run migrations:docker & PID=$!
   wait $PID
-  echo "running tests..."
-  pnpm run test:e2e:docker & PID=$!
-  wait $PID
+
   echo "running docker server..."
   pnpm run dev:docker 
+
+  echo "ENV: $API_ENV is up"
 fi
 
 if [ $API_ENV == "sandbox" ] 
@@ -28,6 +30,9 @@ then
   echo "running migrations..."
   pnpm run migrations & PID=$!
   wait $PID
+
   echo "running sandbox server..."
   pnpm run start 
+
+  echo "ENV: $API_ENV is up"
 fi
