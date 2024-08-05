4d8e145e513a3007090e5f5c6c6d41905684cd5f
nrg-drm <su.re44e@gmail.com>
2024-08-05 00:02:45 -0300

    build: starting from scratch

--- run.sh
+++ run.sh
@@ -4,25 +4,5 @@ echo "running migrations..."
 pnpm run migrations & PID=$!
 wait $PID
 
-echo "set enviroment: echo $API_ENV"
-
-if [ $API_ENV == "local" ] 
-then
-  pnpm run start & PID=$!
-  wait $PID
-
-  echo "local server is up"
-fi
-
-if [ $API_ENV == "sandbox" ] 
-then
-  echo "running migrations..."
-  pnpm run migrations & PID=$!
-  wait $PID
-
-  echo "running sandbox server..."
-  pnpm run start & PID=$!
-  wait $PID
-
-  echo "ENV: $API_ENV is up"
-fi
+echo "running sandbox server..."
+pnpm run start 
